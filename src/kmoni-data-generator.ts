2db1b0f750e6ce5876c5b03883a4d86494cfaf1e
よね/Yone <git@yoneyo.com>
2025-05-09 00:06:14 +0900

    Refactor(KmoniData): Inline date generation logic in reportTime getter

--- src/kmoni-data-generator.ts
+++ src/kmoni-data-generator.ts
@@ -23,7 +23,18 @@ export class KmoniData {
     }
 
     get reportTime(): Date {
-        return KmoniDateGenerator.generateDate({ date: this._data.report_time });
+        function generateDate(date: string): Date {
+            // "YYYY/MM/DD HH:mm:ss"
+            const year = Number(date.slice(0, 4));
+            const month = Number(date.slice(5, 7)) - 1;
+            const day = Number(date.slice(8, 10));
+            const hour = Number(date.slice(11, 13));
+            const minute = Number(date.slice(14, 16));
+            const second = Number(date.slice(17, 19));
+
+            return new Date(year, month, day, hour, minute, second);
+        }
+        return generateDate(this._data.report_time);
     }
 
     get requestTime(): Date {
@@ -32,6 +43,10 @@ export class KmoniData {
 
     get originTime(): Date {
         return KmoniDateGenerator.generateDate({ date: this._data.origin_time });
+    }
+
+    get isWarning(): boolean {
+        return this._data.alertflg === "警報";
     }
 
     get data(): KmoniDataType {
